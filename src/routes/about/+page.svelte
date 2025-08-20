2ea5e4b85536afc6b61d8dabf1e0633555158b6c
Joell Stocchero <jkstocchero@gmail.com>
2025-08-20 17:29:20 +0000

    about and contact page placeholders

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    import Nav from '$lib/components/Nav.svelte';
+    import Footer from '$lib/components/Footer.svelte';
+</script>
+
+<Nav />
+
+<div class="m-24">
+    <h1 class="text-6xl">About page</h1>
+</div>
+
+<Footer />
