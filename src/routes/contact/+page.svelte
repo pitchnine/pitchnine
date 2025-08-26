c966397cdfdc3f8101913d1d16b4eba385bd082d
Joell Stocchero <jkstocchero@gmail.com>
2025-08-26 20:14:32 +0000

    form plopped on contact page

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -2,17 +2,15 @@
     import Nav from '$lib/components/Nav.svelte';
     import Footer from '$lib/components/Footer.svelte';
     import ContactHero from '$lib/components/ContactHero.svelte';
+    import Form from '$lib/components/Form.svelte';
 </script>
 
 <Nav />
 
 <ContactHero />
 
-<div class="m-24">
-    <h1 class="text-6xl">Contact page</h1>
-    <form>
-        <div class="bg-slate-50/30">form fields</div>
-    </form>
+<div>
+    <Form />
 </div>
 
 <Footer />
