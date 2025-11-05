9ab314c2a63343814a18e0faeca52c99d3092f58
Joell Stocchero <jkstocchero@gmail.com>
2025-11-05 16:11:02 +0000

    homepage hero and nav responsive updates to increase negative space and shorten headline

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -8,20 +8,24 @@
 		{ href: '/contact', label: 'Contact' }
 	];
 </script>
-
-<div class="flex items-center justify-between gap-4">
+<nav class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+<div
+		class="flex items-center justify-between py-4
+			   sm:flex-row sm:justify-between
+			   flex-col gap-6 sm:gap-0"
+	>
 	<a href="/" class="logo-link" aria-label="Pitch Nine home">
 		{#if currentPath === '/'}
 			<!-- SELECTED -->
 			
-				<span class="inter text-lg font-bold tracking-tight text-gray-50">PITCH</span><span
-					class="inter text-lg font-bold tracking-tight text-gray-50/60">NINE</span
+				<span class="inter text-xl font-bold tracking-tight text-gray-50">PITCH</span><span
+					class="inter text-xl font-bold tracking-tight text-gray-50/50">NINE</span
 				>
 	
 		{:else}
 			<!-- DEFAULT -->
-			<span class="inter text-lg font-bold tracking-tight text-gray-50/50">PITCH</span><span
-				class="inter text-lg font-bold tracking-tight text-gray-50/25">NINE</span
+			<span class="inter text-xl font-bold tracking-tight text-gray-50/50">PITCH</span><span
+				class="inter text-xl font-bold tracking-tight text-gray-50/25">NINE</span
 			>
 		{/if}
 	</a>
@@ -34,3 +38,4 @@
 		{/each}
 	</div>
 </div>
+</nav>
